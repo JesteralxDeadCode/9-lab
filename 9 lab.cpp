fc7a014f77916131f7c74a21904dfaac35d0f7bf
MaksimCH <deadgamemaker@gmail.com>
2023-05-28 14:26:28 +0300

    end

--- 9 lab.cpp
+++ 9 lab.cpp
@@ -34,13 +34,57 @@ void zad1()
 
 void zad2()
 {
+    string buffer;
+    string strings[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Введите строку #" << i + 1 << " :";
+        cin>>strings[i];
+    }
 
+    for (int i = 0; i < 10; i++)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            if (strings[x] > strings[x + 1])
+            {
+                buffer = strings[x];
+                strings[x] = strings[x + 1];
+                strings[x + 1] = buffer;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout<< endl << strings[i];
+    }
+}
+
+void zad3()
+{
+    string str1 = "Never ", str2 = "Say ", str3 = "Never ", strResult = str1 + str2 + str3;
+    strResult.erase(strResult.size() - 1);
+    strResult += '!';
+    for (int i = 0; i < strResult.size(); i++)
+    {
+        if (isupper(strResult[i]))
+        {
+            strResult[i] = tolower(strResult[i]);
+        }
+    }
+    if (islower(strResult[0]))
+    {
+        strResult[0] = toupper(strResult[0]);
+    }
+
+    cout << strResult;
 }
 
 int main()
 {
     setlocale(LC_ALL, "ru");
     
-    zad1();
+    zad3();
 
 }
